f84f6be45ed08d70190a975b57bf5dbc4bf21e7b
BogomolSergo <breinkiller650@gmail.com>
2017-02-23 23:41:20 +0200

    cvs use by me

--- csv.rb
+++ csv.rb
@@ -7,7 +7,7 @@ class HomeCSV
     end
   end
 
-  def self.write_csv(masive)
+  def self.write_csv(masive) #write a input data
     CSV.open('/home/sergo/Documents/file1.csv', 'wb') do |csv|
       csv << %W(#{masive})
       puts 'File was written!'
