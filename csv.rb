1e8580f78e8e4a5c449ac7b37fd42f839f733b78
BogomolSergo <breinkiller650@gmail.com>
2017-02-23 23:15:01 +0200

    first commit

---
+++ csv.rb
@@ -0,0 +1,31 @@
+require 'csv'
+class HomeCSV
+  @array = []
+  def self.read_csv
+    CSV.foreach('/home/sergo/Documents/file.csv') do |row|
+      print row
+    end
+  end
+
+  def self.write_csv(masive)
+    CSV.open('/home/sergo/Documents/file1.csv', 'wb') do |csv|
+      csv << %W(#{masive})
+      puts 'File was written!'
+    end
+  end
+
+  def self.input
+    puts 'write Name: '
+    name = gets.chomp
+    @array << name
+    puts 'write Age: '
+    age = gets.chomp
+    @array << age
+    puts 'write City: '
+    city = gets.chomp
+    @array << city
+    @array
+  end
+end
+
+HomeCSV.write_csv(HomeCSV.input)
