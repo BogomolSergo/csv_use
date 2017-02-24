913ce261cc77e47a2166e86f51725ec55df7d690
BogomolSergo <breinkiller650@gmail.com>
2017-02-24 23:51:34 +0200

    cvs DZ, all fantasy

--- dz_csv.rb
+++ dz_csv.rb
@@ -1,7 +1,7 @@
 require 'csv'
 class HomeCSV
   @hash = {}
-  def self.read_csv
+  def self.read_csv # Read & build data.
     CSV.foreach('/home/sergo/Documents/file.csv') do |row|
       #my_m = {}
       #months = row[0].split('.')
@@ -12,26 +12,34 @@ class HomeCSV
     return @hash
   end
 
-  def self.detect_month()
-
+  def self.that_oll?(params) # Check month switch.
+    unless @mont == params
+      puts "Temperature month #{@mont}:~ #{@sum_t / @i} C"
+      @mont = params
+      @i = 0
+      @sum_t = 0
+    end
   end
 
-  def self.mdt_temperatures (temp)
-    sum_t = 0
-    sum_t1 = 0
-    i = 0
-    j = 0
+  def self.mdt_temperatures (temp) # Calculations
+    @sum_t = 0
+    @i = 0
+    @mont = '01'
     temp.each_pair do |data, t|
       case data[1]
         when '01'
-          i += 1
-          sum_t += t
+          @i += 1
+          @sum_t += t
         when '02'
-          j += 1
-          sum_t1 += t
+          that_oll?(data[1])
+          @i += 1
+          @sum_t += t
+        when '03'
+          that_oll?(data[1])
+          @i += 1
+          @sum_t += t
       end
     end
-    puts "Temperature of the month:~ #{sum_t / i},  #{sum_t1 / j}"
   end
 end
 HomeCSV.mdt_temperatures(HomeCSV.read_csv)
