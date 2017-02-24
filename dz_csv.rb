0f5ebedbe68e19ce9a78a360a81ddadf13024df0
BogomolSergo <breinkiller650@gmail.com>
2017-02-25 00:08:50 +0200

    cvs DZ, all fantasy, now its finish

--- dz_csv.rb
+++ dz_csv.rb
@@ -1,19 +1,19 @@
 require 'csv'
 class HomeCSV
-  @hash = {}
   def self.read_csv # Read & build data.
+    @hash = {}
     CSV.foreach('/home/sergo/Documents/file.csv') do |row|
-      #my_m = {}
-      #months = row[0].split('.')
-      #my_m[months[0]] = months[1]
+      # my_m = {}
+      # months = row[0].split('.')
+      # my_m[months[0]] = months[1]
       @hash[row[0].split('.')] = row[1].to_i
     end
     puts @hash
-    return @hash
+    @hash
   end
 
   def self.that_oll?(params) # Check month switch.
-    unless @mont == params
+    unless params == @mont
       puts "Temperature month #{@mont}:~ #{@sum_t / @i} C"
       @mont = params
       @i = 0
@@ -21,23 +21,23 @@ class HomeCSV
     end
   end
 
-  def self.mdt_temperatures (temp) # Calculations
-    @sum_t = 0
+  def self.mdt_temperatures(temp)
+    @sum_t = 0 # Calculations
     @i = 0
     @mont = '01'
     temp.each_pair do |data, t|
       case data[1]
-        when '01'
-          @i += 1
-          @sum_t += t
-        when '02'
-          that_oll?(data[1])
-          @i += 1
-          @sum_t += t
-        when '03'
-          that_oll?(data[1])
-          @i += 1
-          @sum_t += t
+      when '01'
+        @i += 1
+        @sum_t += t
+      when '02'
+        that_oll?(data[1])
+        @i += 1
+        @sum_t += t
+      when '03'
+        that_oll?(data[1])
+        @i += 1
+        @sum_t += t
       end
     end
   end
