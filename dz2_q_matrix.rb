4aa1e940af3e414fa8aa65ccdf2d467377cc575b
BogomolSergo <breinkiller650@gmail.com>
2017-02-25 19:01:50 +0200

    HW2 like as finish)

--- dz2_q_matrix.rb
+++ dz2_q_matrix.rb
@@ -16,21 +16,20 @@ class Dz2QMatrix
     @range
   end
 
-	def q_matrix
-		matrix = @range
-		matrix2 = []
-		i = 0
-		j = 0
-		(matrix.size).times do
-			matrix.each_index do |d|
-				matrix2 << matrix[d+i]
-				i = -1 if i == 3
-			end
-			i += 1
-			print "#{matrix2} \n"
-			matrix2.clear
-		end
-	end
+  def q_matrix
+    matrix = @range
+    matrix2 = []
+    i = 0
+    matrix.size.times do
+      matrix.each_index do |d|
+        matrix2 << matrix[d + i]
+        i = -matrix.size if i + d == matrix.size - 1
+      end
+      i += 1
+      print "#{matrix2} \n"
+      matrix2.clear
+    end
+  end
 end
 
 m = Dz2QMatrix.new(10)
