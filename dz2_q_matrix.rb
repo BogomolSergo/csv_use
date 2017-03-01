e623d91eeb5e6c86c56ea688e1e6825e5bbc120f
BogomolSergo <breinkiller650@gmail.com>
2017-03-01 13:33:40 +0200

    HW2 like better)

--- dz2_q_matrix.rb
+++ dz2_q_matrix.rb
@@ -16,21 +16,18 @@ class Dz2QMatrix
     @range
   end
 
-  def q_matrix
-    matrix = @range
-    matrix2 = []
-    i = 0
-    matrix.size.times do
-      matrix.each_index do |d|
-        matrix2 << matrix[d + i]
-        i = -matrix.size if i + d == matrix.size - 1
-      end
-      i += 1
-      print "#{matrix2} \n"
-      matrix2.clear
-    end
+  def build_this_shit(matrix = @range, matrix2 = [], i = 0)
+	  matrix.size.times do
+		  matrix.each_index do |d|
+			  matrix2 << matrix[d + i]
+			  i = -matrix.size if i + d == matrix.size - 1
+		  end
+		  i += 1
+		  print "#{matrix2} \n"
+		  matrix2.clear
+	  end
   end
 end
 
 m = Dz2QMatrix.new(10)
-m.q_matrix
+m.build_this_shit
