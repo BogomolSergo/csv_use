777847114cef3a07b378be368a604a4954109847
BogomolSergo <breinkiller650@gmail.com>
2017-03-01 14:24:09 +0200

    HW2 like better)

--- dz2_q_matrix.rb
+++ dz2_q_matrix.rb
@@ -1,26 +1,27 @@
 class Dz2QMatrix
   def initialize(n)
-    @n = n
+    @n = n # max = 10 digits
     @n > 10 ? 'Object cant be >10' : to_simple?(@n)
   end
 
   def to_simple?(n)
-    @range = [2]
-    for i in 0..n
+    @range = [2] # First simple digit.
+    (0..n).each do |i|
       next unless i > 1
-      for j in 2..i
+      (2..i).each do |j|
         break if (i % j).zero?
-        @range << i if j + 1 == i
+        @range << i if j.next == i
       end
     end
+    @size = @range.size
     @range
   end
 
-  def build_this_shit(matrix = @range, matrix2 = [], i = 0)
+  def build_this_shit(matrix = @range, size = @size, matrix2 = [], i = 0)
     matrix.size.times do
       matrix.each_index do |d|
         matrix2 << matrix[d + i]
-        i = -matrix.size if i + d == matrix.size - 1
+        i = -size if i + d == size - 1
       end
       i += 1
       print "#{matrix2} \n"
