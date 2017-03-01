85b651c1ac30e750832fe6dce612935e643cd566
BogomolSergo <breinkiller650@gmail.com>
2017-03-01 13:20:21 +0200

    HW2 like as finish)

--- dz3_csv.rb
+++ dz3_csv.rb
@@ -3,9 +3,6 @@ class HomeCSV
   def self.read_csv # Read & build data.
     @hash = {}
     CSV.foreach('/home/sergo/Documents/file.csv') do |row|
-      # my_m = {}
-      # months = row[0].split('.')
-      # my_m[months[0]] = months[1]
       @hash[row[0].split('.')] = row[1].to_i
     end
     puts @hash
@@ -26,19 +23,9 @@ class HomeCSV
     @i = 0
     @month = '01'
     temp.each_pair do |data, t|
-      case data[1]
-      when '01'
-        @i += 1
-        @sum_t += t
-      when '02'
-        that_oll?(data[1])
-        @i += 1
-        @sum_t += t
-      when '03'
-        that_oll?(data[1])
-        @i += 1
-        @sum_t += t
-      end
+      @i += 1
+      @sum_t += t
+      that_oll?(data[1])
     end
   end
 end
